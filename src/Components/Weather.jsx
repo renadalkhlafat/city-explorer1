f81b8b7ff6fb6a89b2715f8bbbef360b48439d0b
renadalkhlafat <renadsalem8888@gmail.com>
2021-08-11 01:34:22 +0300

    lab08

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -6,13 +6,14 @@ class Weather extends Component {
     render() { 
         return ( 
             <Col >
-            <Card>
+            <Card className='m-2' style={{width:'12rem'}}>
                 <Card.Header style={{background:'#FFC107'}}>
                     {this.props.date}
                 </Card.Header>
-                <Card.Body>
-                    {this.props.description==='Scattered clouds'?<img src='https://www.kindpng.com/picc/m/122-1227285_scattered-clouds-weather-symbol-hd-png-download.png' alt='..' width="45px"/>:<img src='https://i.pinimg.com/originals/8a/93/5d/8a935da3c85c3981a0b3eabfdc6221b3.png' alt='..' width="45px"/>}
-                    {this.props.description}
+                <Card.Body >
+                    {this.props.description.includes('Scattered clouds')|| this.props.description.includes('Few clouds')?<img src='https://www.kindpng.com/picc/m/122-1227285_scattered-clouds-weather-symbol-hd-png-download.png' alt='..' width="45px"/>:<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTas2ehcpXNqPfEoAiCXHrfGJ6A7NSq4K3WsQ&usqp=CAU' alt='..' width="45px"/>}
+                    <p style={{float:"right" ,fontWeight:"800",fontSize:"20px"}}>{this.props.temp}<sup>°</sup></p>
+                  <Card.Text>  {this.props.description}</Card.Text>
                 </Card.Body>
                 
             </Card>
